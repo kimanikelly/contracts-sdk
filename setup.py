b6a881aab8425d62058fbde6df120967fc1d3416
Kimani Kelly <kimanikelly@Kimanis-MacBook-Pro.local>
2022-12-26 16:17:26 -0500

    Update .gitignore

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.0.0"
+DESCRIPTION = "Smart Contract Software Development Kit"
+LONG_DESCRIPTION = "A Software Development Kit interacting with Solidity Smart Contracts, aiming to build Off-Chain applications on Ethereum compatible protocols."
+
+setup(
+    name="Smart Contract SDK",
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    author="Kimani Kelly",
+    author_email="kimanikelly95@gmail.com",
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[],
+    keywords="Smart Contract SDK"
+)
