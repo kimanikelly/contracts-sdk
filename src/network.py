f07643790d4a3975a3505da3e2714ab0f8d83b93
Kimani Kelly <kimanikelly@Kimanis-MacBook-Pro.local>
2022-12-26 11:20:00 -0500

    Network Added comments to the Network file

--- src/network.py
+++ src/network.py
@@ -4,28 +4,40 @@ from src.contractApi import *
 from src.local_addresses import *
 
 
-def token_addresses(network_id: int):
+def token_addresses(network_id: int) -> str:
     """ Returns the Token.sol address associated with the network_id
 
     Args:
-        network_id (int): _description_
+        network_id (int): The numeric value used to connect with Ethereum compatible protocols
 
     Returns:
-        _type_: _description_
+        str: The Token.sol address
     """
+
+    # Checks if the network routes to Goerli testnet
     if network_id == 5:
         return token_goerli_address
 
+    # Checks if the network routes to Ganache locally
     if network_id == 1337:
-
         return addresses["token_local_address"]
 
 
-def ttBank_addresses(network_id: int):
+def ttBank_addresses(network_id: int) -> str:
+    """ Returns the TTBank.sol address associated with the network_id
 
+    Args:
+        network_id (int): The numeric value used to connect with Ethereum compatible protocols
+
+    Returns:
+        str: The TTBank.sol address
+    """
+
+    # Checks if the network routes to Goerli testnet
     if network_id == 5:
         return ttBank_goerli_address
 
+    # Checks if the network routes to Ganache locally
     if network_id == 1337:
 
         return addresses["ttBank_local_address"]
