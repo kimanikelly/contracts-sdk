46fe121ac0db6e0bba58878b27dcec6f9f8f043d
Kimani Kelly <kimanikelly@Kimanis-MacBook-Pro.local>
2023-03-17 10:40:28 -0400

    Added the new contract api deployed with Heroku

--- src/contract_api.py
+++ src/contract_api.py
@@ -1,19 +1,19 @@
 import requests
 
 token_goerli_address = requests.get(
-    "http://ec2-34-203-42-249.compute-1.amazonaws.com/token").json()['addresses']['goerli']
+    "https://smart-contracts-api-node.herokuapp.com/token").json()['addresses']['goerli']
 
 token_abi = requests.get(
-    "http://ec2-34-203-42-249.compute-1.amazonaws.com/token").json()['abi']
+    "https://smart-contracts-api-node.herokuapp.com/token").json()['abi']
 
 token_bytecode = requests.get(
-    "http://ec2-34-203-42-249.compute-1.amazonaws.com/token").json()['bytecode']
+    "https://smart-contracts-api-node.herokuapp.com/token").json()['bytecode']
 
 ttBank_goerli_address = requests.get(
-    "http://ec2-34-203-42-249.compute-1.amazonaws.com/ttbank").json()['addresses']['goerli']
+    "https://smart-contracts-api-node.herokuapp.com/ttBank").json()['addresses']['goerli']
 
 ttBank_abi = requests.get(
-    "http://ec2-34-203-42-249.compute-1.amazonaws.com/ttbank").json()['abi']
+    "https://smart-contracts-api-node.herokuapp.com/ttBank").json()['abi']
 
 ttBank_bytecode = requests.get(
-    "http://ec2-34-203-42-249.compute-1.amazonaws.com/ttbank").json()['bytecode']
+    "https://smart-contracts-api-node.herokuapp.com/ttBank").json()['bytecode']
